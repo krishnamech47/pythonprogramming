aab8b820ccdddb31eb0b6af5f9a52179497f22a0
Krishna Venkatesh <51222689+krishnamech47@users.noreply.github.com>
2019-07-12 04:23:12 -0700

    Update numadd.py

--- numadd.py
+++ numadd.py
@@ -1,6 +1,6 @@
-n,k=map(int,input.split())
+n,k=map(int,input().split())
 sum=0
-a=list(map(int,input.split())
-for i in range (0,k):
+a=list(map(int,input().split())
+for i in range(0,k):
  sum=sum+a[i]
 print(sum)
