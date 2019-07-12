30d03bf2cfef2fd9a42151acef343982fed4d17e
Krishna Venkatesh <51222689+krishnamech47@users.noreply.github.com>
2019-07-12 20:45:57 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,4 +1,4 @@
-import math as m
+import math 
 n=int(input())
 temp=n
 arm=n
@@ -6,12 +6,12 @@ p=0
 sum=0
 rem=0
 while n>0 :
- n=n/10
+ n=n//10
  p=p+1
 while temp>0 :
  rem=temp%10
- sum=sum+m.pow(rem,p)
- temp=temp/10
+ sum=sum+math.pow(rem,p)
+ temp=temp//10
 if arm==sum :
  print("yes")
 else :
