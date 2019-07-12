7d00b5459755bdbeb2ab7d4c60435b8cddafe105
Krishna Venkatesh <51222689+krishnamech47@users.noreply.github.com>
2019-07-12 03:41:06 -0700

    Update larg.py

--- larg.py
+++ larg.py
@@ -1,12 +1,12 @@
 i =int(input())
 j=int(input())
 k=int(input())
-if i>j :
- if(i>k) :
+if i>=j :
+ if(i>=k) :
   print(i)
  else :
   print(k)
-elif j>k :
+elif j>=k :
  print(j)
 else :
  print(k)
