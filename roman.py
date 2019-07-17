189ee38e7b9c377ff12c0f172d94e86e0e829031
Krishna Venkatesh <51222689+krishnamech47@users.noreply.github.com>
2019-07-17 19:55:48 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,8 @@
+n=input().upper()
+list1=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIV','XX']
+list2=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
+for i in range(0,20):
+ if(n==list1[i]):
+  {
+   print(list2[i])
+   }
