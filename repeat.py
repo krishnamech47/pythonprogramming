29c83bed25c2ff5842edae535884edbc684fb139
Krishna Venkatesh <51222689+krishnamech47@users.noreply.github.com>
2019-07-23 15:26:50 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -7,6 +7,8 @@ for i in range(0,len(a)):
         if((a[i]==a[j]) and (a[i] not in c)):
             c.append(a[i])
             b=b+1 
+if(len(c)==0):
+    print("unique")
 for k in range(0,len(c)):
         print(c[k],end=" ")
     
